20ad962882472bd82f7ed864a4e86a7421a4c58a
dvisionst <62859441+dvisionst@users.noreply.github.com>
2021-11-05 15:44:34 -0700

    Update main.py
    Full working code, both weather and cumulative run sections are complete

--- main.py
+++ main.py
@@ -22,27 +22,20 @@ def pair_list_filter(v_obj, asc_list_indexes):
         i += 1
     return speed_pairs_f_list
 
+
 WLTP_HIGH_B_SPEED = 135
 NEDC_HIGH_B_SPEED = 125
 HIGH_E_SPEED = 55
 
 LOW_B_SPEED = 85
 LOW_E_SPEED = 15
-HIGH_B_SPEED = 0
-test_type = input("For WLTP test press 'w', for NEDC test press 'n': \n").lower()
-if test_type == 'w':
-    HIGH_B_SPEED = WLTP_HIGH_B_SPEED
-elif test_type == 'n':
-    HIGH_B_SPEED = NEDC_HIGH_B_SPEED
+
 test_runs = input("Was this test done with Split Runs? Type 'y' for YES, Type 'n' for NO:  \n").lower()
-
-
 coast_down_processor_on = True
 while coast_down_processor_on:
-
     if test_runs == "y":
         vbox_high = Vbox()
-        h_pair_start_array = vbox_high.start_trig_times(high_end_speed=HIGH_B_SPEED)
+        h_pair_start_array = vbox_high.start_trig_times(high_end_speed=WLTP_HIGH_B_SPEED)
         h_pair_stop_array = vbox_high.stop_trig_times(low_end_speed=HIGH_E_SPEED)
         sorted_high_list = cd.total_index_sort(start_list=h_pair_start_array, stop_list=h_pair_stop_array)
         filtered_h_pair_indices = pair_list_filter(vbox_high, sorted_high_list)
@@ -124,77 +117,16 @@ while coast_down_processor_on:
             print("You have terminated the Coastdown Processor.")
             coast_down_processor_on = False
 
+data = pd.read_csv("vbox_results_runs.csv")
 
-
-
-
-
-data = pd.read_csv("vbox_results_all_runs2.csv")
-
-print(data.Heading)
-print(type(data.iloc[0].Heading))
 results = Results()
 results.high_run_pull()
 results.low_run_pull()
-
-x = results.odd_run_fix()[0]
-y = results.odd_run_fix()[1]
-z = results.odd_run_fix()[2]
-
-if results.tuple_creation_ideal():
-    pairs_list = results.tuple_creation_ideal()
-elif not results.tuple_creation_ideal():
-    pairs_list = cd.updating_pairs(odd_list=x, index=y)
-    print(pairs_list)
-    if z > 5:
-        results.high_index = pairs_list
-    elif z < 5:
-        results.low_index = pairs_list
-cume_tup = results.tuple_creation_ideal()
-print(cume_tup)
-i = 0
-j = 1
-k = 0
-total_high = 0
-first_one_pair_cume = []
-while i < len(cume_tup):
-    while k < 5:
-        high_sum = results.data.iloc[cume_tup[i]][results.high_dict["high"][k]]
-        total_high += high_sum
-        print(total_high)
-        first_one_pair_cume.append(total_high)
-        k += 1
-    i += 1
-    break
-
-
-
-i = 0
-n = 5
-m = 0
-
-while i < 3:
-    h_v = results.data.iloc[cume_tup[0]][results.high_dict["high"][n]]
-    l_v = results.data.iloc[cume_tup[1]][results.low_dict["low"][m]]
-    sm = h_v + l_v
-    aveee = sm/2
-    total_high += aveee
-    print(total_high)
-    first_one_pair_cume.append(total_high)
-    i += 1
-    n += 1
-    m += 1
-
-
-total_low = first_one_pair_cume[-1]
-k = 3
-while j < len(cume_tup):
-
-    while k < 7:
-        low_sum = results.data.iloc[cume_tup[j]][results.low_dict["low"][k]]
-        total_high += low_sum
-        print(total_high)
-        k += 1
-    i += 1
-    break
-
+amount_of_pairs = results.loop_index()
+results.output_pairs_list()
+results.top_vel_accume()
+results.mid_ave_accume()
+results.low_vel_accume()
+results.final_dataframe()
+df = pd.DataFrame(results.columns)
+df.to_csv("cumulative_times_results.csv")
