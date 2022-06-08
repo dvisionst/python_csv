d5da4a217f0114ccde280c24a8afbc1f00a186ac
dvisionst <62859441+dvisionst@users.noreply.github.com>
2022-06-08 10:03:05 -0700

    Update vbox.py

--- vbox.py
+++ vbox.py
@@ -1,39 +1,92 @@
+# This class is used for the raw VBOX data collected during the coastdown. It creates a Dataframe that allows the
+# main program to search for key timestamp values using the start and stop velocities during coastdown.
+
 import pandas
 
 
 class Vbox:
 
     def __init__(self):
-        self.data = pandas.read_csv("coast_data.csv", low_memory=False)
+        self.data = pandas.read_csv("coast_data_IN.csv", low_memory=False)
         self.time_c = self.data.velocity
         self.t_stamp_series = []
         self.filtered_time_list = []
-        self.high_speed_indices = []
-        self.low_speed_indices = []
+        self.splits_indices = []
 
-    def start_trig_times(self, high_end_speed):
+    def mod_times(self, high_leg_begin, high_leg_end, low_leg_begin, low_leg_end):
+        leg_index = 1
         k = 0
         i = 1
         j = 10
         while j < len(self.time_c):
-            if self.time_c[j] < self.time_c[i] < high_end_speed <= self.time_c[k]:
-                self.high_speed_indices.append(i)
-            k += 1
-            i += 1
-            j += 1
-        return self.high_speed_indices
+            while leg_index % 2 != 0 and j < len(self.time_c):
+                trigger = 1
+                count = 0
+                while j < len(self.time_c) and count < 4:
+                    if trigger % 2 != 0:
+                        if self.time_c[j] < self.time_c[i] < high_leg_begin <= self.time_c[k]:
+                            self.splits_indices.append(i)
+                            count += 1
+                            trigger += 1
+                        k += 1
+                        i += 1
+                        j += 1
+                    elif trigger % 2 == 0:
+                        if self.time_c[j] < self.time_c[i] < high_leg_end <= self.time_c[k]:
+                            self.splits_indices.append(i)
+                            count += 1
+                            trigger += 1
+                        k += 1
+                        i += 1
+                        j += 1
+                leg_index += 1
+            while leg_index % 2 == 0 and j < len(self.time_c):
+                trigger = 1
+                count = 0
+                while j < len(self.time_c) and count < 4:
+                    if trigger % 2 != 0:
+                        if self.time_c[j] < self.time_c[i] < low_leg_begin <= self.time_c[k]:
+                            self.splits_indices.append(i)
+                            count += 1
+                            trigger += 1
+                        k += 1
+                        i += 1
+                        j += 1
+                    elif trigger % 2 == 0:
+                        if self.time_c[j] < self.time_c[i] < low_leg_end <= self.time_c[k]:
+                            self.splits_indices.append(i)
+                            count += 1
+                            trigger += 1
+                        k += 1
+                        i += 1
+                        j += 1
+                leg_index += 1
+        return self.splits_indices
 
-    def stop_trig_times(self, low_end_speed):
+    def mod_times_single(self, starting_velocity, ending_velocity):
         k = 0
         i = 1
         j = 10
+
+        trigger = 1
         while j < len(self.time_c):
-            if self.time_c[j] < self.time_c[i] < low_end_speed <= self.time_c[k]:
-                self.low_speed_indices.append(i)
-            k += 1
-            i += 1
-            j += 1
-        return self.low_speed_indices
+
+            if trigger % 2 != 0:
+                if self.time_c[j] < self.time_c[i] < starting_velocity <= self.time_c[k]:
+                    self.splits_indices.append(i)
+                    trigger += 1
+                k += 1
+                i += 1
+                j += 1
+            elif trigger % 2 == 0:
+
+                if self.time_c[j] < self.time_c[i] < ending_velocity <= self.time_c[k]:
+                    self.splits_indices.append(i)
+                    trigger += 1
+                k += 1
+                i += 1
+                j += 1
+        return self.splits_indices
 
     def time_stamp(self, index_list):
         i = 0
@@ -42,6 +95,3 @@ class Vbox:
             self.filtered_time_list.append(temp_var)
             i += 1
         return self.filtered_time_list
-
-
-
